e0ed98669aac4436b0fcd512f1924d9a7b93327f
Juani <jperezdichiazza@gmail.com>
2024-11-15 11:29:10 -0300

    fix: add _id to notification return schema

--- src/db/models/Notification.js
+++ src/db/models/Notification.js
@@ -12,7 +12,7 @@ const notificationSchema = new mongoose.Schema({
 notificationSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     // returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
+    // delete returnedObject._id;
     delete returnedObject.__v;
   },
 });
